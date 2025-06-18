7c990f2321bca8f96af4c0b5d066d0c95639038d
Vikky yadav <146602351+HeartKing88@users.noreply.github.com>
2025-06-18 12:05:02 +0530

    Update __init__.py

--- SystemMusic/__init__.py
+++ SystemMusic/__init__.py
@@ -1,1 +1,26 @@
+from SystemMusic.core.bot import System
+from SystemMusic.core.dir import dirr
+from SystemMusic.core.git import git
+from SystemMusic.core.userbot import Userbot
+from SystemMusic.misc import dbb, heroku
 
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = System()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
