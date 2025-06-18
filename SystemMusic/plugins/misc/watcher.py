c6a5aa164517235efc490f19d91988e1ce6cd8f9
Vikky yadav <146602351+HeartKing88@users.noreply.github.com>
2025-06-18 13:15:52 +0530

    Update watcher.py

--- SystemMusic/plugins/misc/watcher.py
+++ SystemMusic/plugins/misc/watcher.py
@@ -1,1 +1,14 @@
+from pyrogram import filters
+from pyrogram.types import Message
 
+from SystemMusic import app
+from SystemMusic.core.call import System
+
+welcome = 20
+close = 30
+
+
+@app.on_message(filters.video_chat_started, group=welcome)
+@app.on_message(filters.video_chat_ended, group=close)
+async def welcome(_, message: Message):
+    await System.stop_stream_force(message.chat.id)
