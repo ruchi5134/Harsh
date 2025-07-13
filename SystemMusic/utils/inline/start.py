e4045f677becdd0f0123feaea9b962ec9f79fb2d
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2025-07-13 22:22:32 +0530

    Update start.py

--- SystemMusic/utils/inline/start.py
+++ SystemMusic/utils/inline/start.py
@@ -27,28 +27,12 @@ def private_panel(_):
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text="❍ ʀᴇᴘᴏ ❍", callback_data="gib_source"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
-
-
-
-def music_start_panel(_):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text="💠 ᴀᴅᴅ ᴍᴇ ɪɴ ɴᴇᴡ ɢʀᴏᴜᴘ 💠",
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(text="˹❍ᴡɴᴇꝛ˼", user_id=config.OWNER_ID),
-            InlineKeyboardButton(text="❍ sᴜᴘᴘᴏʀᴛ ❍", callback_data="support"),
-        ],
-        [InlineKeyboardButton(text="❍ ᴍᴏᴅᴇ ❍", callback_data="feature")],
-    ]
-    return buttons
+    
